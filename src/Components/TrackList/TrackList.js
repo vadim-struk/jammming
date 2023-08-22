d9e0c7999b39586cb1362366d2f5ba92a998f43b
vadim-struk <117313404+vadim-struk@users.noreply.github.com>
2023-08-22 12:31:52 +0300

    Fixed bug with Track module address 4

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -7,7 +7,7 @@ class TrackList extends React.Component {
         return(
             <div className="TrackList">
                 <Track />
-            {/* <!-- You will add a map method that renders a set of Track components  --> */}
+           //{/* <!-- You will add a map method that renders a set of Track components  --> */}
         </div>
       )
     }
