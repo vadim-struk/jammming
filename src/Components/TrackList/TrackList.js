4d13c8c16f0bb96ddb4a5272bec65db573e136c4
vadim-struk <117313404+vadim-struk@users.noreply.github.com>
2023-08-22 12:34:55 +0300

    asdfasdgsag

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './TrackList.css';
-import Track from "../Track/Track";
+import Track from "../Track/Track.js";
 
 class TrackList extends React.Component {
     render(){
